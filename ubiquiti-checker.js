a99bc7f182b145f452153e85d469971056d94a22
Tyler Otto <tyler.otto@loadsmart.com>
2022-02-03 17:09:54 -0700

    Fix script name

--- ubiquiti-checker.js
+++ ubiquiti-checker.js
@@ -3,11 +3,17 @@ const {exec} = require("child_process");
 const refreshRate = 10000;
 const urls = [
     'https://store.ui.com/collections/early-access/products/dream-router-ea',
-    'https://store.ui.com/collections/unifi-protect/products/unifi-video-g3-flex-camera',
+    'https://store.ui.com/collections/unifi-protect/products/camera-g4-instant-ea',
+    'https://store.ui.com/collections/unifi-protect/products/g4-doorbell-pro-ea',
+    'https://store.ui.com/collections/early-access/products/dream-router-ea',
+    'https://store.ui.com/collections/early-access/products/g4-doorbell-pro-poe-adapter-ea',
+    // 'https://store.ui.com/collections/unifi-protect/products/unifi-protect-ai-360',
+    // 'https://store.ui.com/collections/unifi-protect/products/unifi-video-g3-flex-camera',
     // 'https://store.ui.com/collections/unifi-protect/products/unifi-protect-g3-instant-camera',
-    // 'https://store.ui.com/collections/unifi-protect/products/camera-g4-instant-ea',
     // 'https://store.ui.com/collections/unifi-protect/products/unifi-video-camera-g3-bullet',
-    // 'https://store.ui.com/collections/unifi-protect/products/uvc-g4-dome'
+    // 'https://store.ui.com/collections/unifi-protect/products/uvc-g4-dome',
+    // 'https://store.ui.com/collections/unifi-network-switching/products/usw-flex',
+    // 'https://store.ui.com/collections/unifi-protect/products/uvc-g4-doorbell',
 ];
 
 (async () => {
@@ -34,7 +40,9 @@ const urls = [
         if (item.available || item.inventory_quantity > 0) {
             setInterval(() => {
                 console.log(`${item.name} IS IN STOCK! (${item.inventory_quantity})`);
-                exec('afplay mixkit-fast-small-sweep-transition-166.mp3')
+                try {
+                    exec('afplay mixkit-fast-small-sweep-transition-166.mp3')
+                } catch (e) {}
             }, 2500);
         }
         
